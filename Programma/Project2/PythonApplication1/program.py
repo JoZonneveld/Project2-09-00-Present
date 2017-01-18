286549fcd5808e3a5992c2f776d6181b57ef9fb0
ShreyShrey96 <shreyasvp96@gmail.com>
2017-01-18 10:39:42 +0100

    applied start button

--- Programma/Project2/PythonApplication1/program.py
+++ Programma/Project2/PythonApplication1/program.py
@@ -32,6 +32,30 @@ class quit_button():
         if click[0] == 1:
                 pygame.quit()
 
+class start_button():
+    def text_objects(self, text, font):
+        textSurface = font.render(text, True, (0,0,0))
+        return textSurface, textSurface.get_rect()
+    
+    def draw_button(self, screen):
+        
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        if 300+100 > mouse[0] > 300 and 450+100 > mouse[1] > 450:
+            pygame.draw.rect(screen, hover_green, (300, 450, 100, 50))
+            
+        else:
+            pygame.draw.rect(screen,green, (300, 450, 100, 50))
+
+        smallText = pygame.font.Font("freesansbold.ttf",14)
+        textSurf, textRect = self.text_objects("Start", smallText)
+        textRect.center = ( (300+(50/2)), (450+(50/2)) )
+        screen.blit(textSurf, textRect)
+
+        if click[0] == 1:
+                pass
+
 def program():
     pygame.init()
 
@@ -43,10 +67,13 @@ def program():
     screen = pygame.display.set_mode(size)
 
     quit = quit_button()
+
+    start = start_button()
     
     while process_events():
         screen.fill(Deep_Sky_Blue)
 
         quit.draw_button(screen)
+        start.draw_button(screen)
 
         pygame.display.flip()
