117465db668f6a8d1579d67c1129c0352f7e71d2
JoZonneveld <joostz14@gmail.com>
2017-01-18 14:47:49 +0100

    nieuwe buttons

--- Programma/Project2/PythonApplication1/program.py
+++ Programma/Project2/PythonApplication1/program.py
@@ -19,6 +19,51 @@ class Background(pygame.sprite.Sprite):
 
 BackGround = Background('gif/water.jpg', [0,0])
 
+class start_button():
+    def text_objects(self, text, font):
+        textSurface = font.render(text, True, (0, 0, 0))
+        return textSurface, textSurface.get_rect()
+
+    def draw_button(self, screen, x, y, b, h):
+
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        if x + b > mouse[0] > x and y + h > mouse[1] > y:  # hor, vert
+            pygame.draw.rect(screen, hover_grey, (x, y, b, h))  # hor, vert, length, height
+        else:
+            pygame.draw.rect(screen, grey, (x, y, b, h))
+
+        smallText = pygame.font.Font("freesansbold.ttf", 20)
+        textSurf, textRect = self.text_objects("Start", smallText)
+        textRect.center = ((475 + (50 / 2)), (260 + (50 / 2)))
+        screen.blit(textSurf, textRect)
+        if click[0] == 1:
+            pass
+
+class setting_button():
+    def text_objects(self, text, font):
+        textSurface = font.render(text, True, (0, 0, 0))
+        return textSurface, textSurface.get_rect()
+
+    def draw_button(self, screen, x, y, b, h):
+
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        if x + b > mouse[0] > x and y + h > mouse[1] > y:  # hor, vert
+            pygame.draw.rect(screen, hover_grey, (x, y, b, h))  # hor, vert, length, height
+        else:
+            pygame.draw.rect(screen, grey, (x, y, b, h))
+
+        smallText = pygame.font.Font("freesansbold.ttf", 20)
+        textSurf, textRect = self.text_objects("Setting", smallText)
+        textRect.center = ((475 + (50 / 2)), (360 + (50 / 2)))
+        screen.blit(textSurf, textRect)
+
+        if click[0] == 1:
+            pass
+
 class quit_button():
     def text_objects(self, text, font):
         textSurface = font.render(text, True, black)
@@ -42,28 +87,7 @@ class quit_button():
         if click[0] == 1:
             pygame.quit()
 
-class start_button():
-    def text_objects(self, text, font):
-        textSurface = font.render(text, True, (0, 0, 0))
-        return textSurface, textSurface.get_rect()
 
-    def draw_button(self, screen, x, y, b, h):
-
-        mouse = pygame.mouse.get_pos()
-        click = pygame.mouse.get_pressed()
-
-        if x + b > mouse[0] > x and y + h > mouse[1] > y:  # hor, vert
-            pygame.draw.rect(screen, hover_grey, (x, y, b, h))  # hor, vert, length, height
-        else:
-            pygame.draw.rect(screen, grey, (x, y, b, h))
-
-        smallText = pygame.font.Font("freesansbold.ttf", 20)
-        textSurf, textRect = self.text_objects("Start", smallText)
-        textRect.center = ((475 + (50 / 2)), (335 + (50 / 2)))
-        screen.blit(textSurf, textRect)
-
-        if click[0] == 1:
-            pass
 
 def program():
     pygame.init()
@@ -75,17 +99,22 @@ def program():
 
     screen = pygame.display.set_mode(size)
 
-    quit = quit_button()
     start = start_button()
+    setting = setting_button()
+    quit = quit_button()
+
 
 
     start = start_button()
 
     while process_events():
+        pygame.display.set_caption('Battleport')
+
         screen.fill(black)
         screen.blit(BackGround.image, BackGround.rect)
 
-        start.draw_button(screen, 350, 325, 300, 70)
+        start.draw_button(screen, 350, 250, 300, 70)
+        setting.draw_button(screen, 350, 350, 300, 70)
         quit.draw_button(screen, 350, 450, 300, 70)
 
 
