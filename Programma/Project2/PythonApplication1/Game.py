311a46c40973cf60d05508e5ebdf05eb36e75ae9
JoZonneveld <joostz14@gmail.com>
2017-01-19 11:45:30 +0100

    New programe, with windows

--- Programma/Project2/PythonApplication1/Game.py
+++ Programma/Project2/PythonApplication1/Game.py
@@ -1,4 +1,158 @@
-from program import *
+from Color import *
 import pygame
 pygame.init()
-program()
+
+#game buttons
+class Button:
+    def text_objects(self, text, font):
+        textSurface = font.render(text, True, (0, 0, 0))
+        return textSurface, textSurface.get_rect()
+
+    def Start(self, screen, x, y, b, h):
+
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        if x + b > mouse[0] > x and y + h > mouse[1] > y:  # hor, vert
+            pygame.draw.rect(screen, hover_grey, (x, y, b, h))  # hor, vert, length, height
+        else:
+            pygame.draw.rect(screen, grey, (x, y, b, h))
+
+        if x + b > mouse[0] > x and y + h > mouse[1] > y and click[0] == 1:
+            Game()
+
+        smallText = pygame.font.Font("freesansbold.ttf", 20)
+        textSurf, textRect = self.text_objects("Start", smallText)
+        textRect.center = ((475 + (50 / 2)), (260 + (50 / 2)))
+        screen.blit(textSurf, textRect)
+
+    def Rules(self, screen, x, y, b, h):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        if x + b > mouse[0] > x and y + h > mouse[1] > y:
+            pygame.draw.rect(screen, hover_grey, (x, y, b, h))
+        else:
+            pygame.draw.rect(screen, grey, (x, y, b, h))
+
+        smallText = pygame.font.Font("freesansbold.ttf", 18)
+        textSurf, textRect = self.text_objects("Rules", smallText)
+        textRect.center = ((47 + (50 / 2)), (35 + (50 / 2)))
+        screen.blit(textSurf, textRect)
+
+        if x + b > mouse[0] > x and y + h > mouse[1] > y and click[0] == 1:
+            Rules()
+
+    def Setting(self, screen, x, y, b, h):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        if x + b > mouse[0] > x and y + h > mouse[1] > y:  # hor, vert
+            pygame.draw.rect(screen, hover_grey, (x, y, b, h))  # hor, vert, length, height
+        else:
+            pygame.draw.rect(screen, grey, (x, y, b, h))
+
+        smallText = pygame.font.Font("freesansbold.ttf", 20)
+        textSurf, textRect = self.text_objects("Settings", smallText)
+        textRect.center = ((475 + (50 / 2)), (360 + (50 / 2)))
+        screen.blit(textSurf, textRect)
+
+        if x + b > mouse[0] > x and y + h > mouse[1] > y and click[0] == 1:
+            Settings()
+
+    def Exit(self, screen, x, y, b, h):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        if x + b > mouse[0] > x and y + h > mouse[1] > y:
+            pygame.draw.rect(screen, hover_grey, (x, y, b, h))
+        else:
+            pygame.draw.rect(screen, grey, (x, y, b, h))
+
+        smallText = pygame.font.Font("freesansbold.ttf", 20)
+        textSurf, textRect = self.text_objects("Exit", smallText)
+        textRect.center = ((475 + (50 / 2)), (460 + (50 / 2)))
+        screen.blit(textSurf, textRect)
+
+        if x + b > mouse[0] > x and y + h > mouse[1] > y and click[0] == 1:
+            Quit()
+
+#game width
+width = 1024
+height = 768
+size = (width, height)
+
+screen = pygame.display.set_mode(size)
+button = Button()
+
+
+def process_events():
+    pygame.init()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return False
+    return True
+
+
+class Background(pygame.sprite.Sprite):
+    def __init__(self, image_file, location):
+        pygame.sprite.Sprite.__init__(self)  # call Sprite initializer
+        self.image = pygame.image.load(image_file)
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
+
+
+BackGround = Background('gif/water.jpg', [0, 0])
+
+def main_screen():
+    while process_events():
+        pygame.display.set_caption('Battleport')
+        screen.fill(black)
+        screen.blit(BackGround.image, BackGround.rect)
+
+        button.Start(screen, 350, 250, 300, 70)
+        button.Rules(screen, 25, 25, 100, 70)
+        button.Setting(screen, 350, 350, 300, 70)
+        button.Exit(screen, 350, 450, 300, 70)
+        pygame.display.update()
+
+    pygame.quit()
+
+def Rules():
+    while process_events():
+        pygame.display.set_caption('Battleport')
+        screen.fill(black)
+        screen.blit(BackGround.image, BackGround.rect)
+
+
+        pygame.display.update()
+
+def Game():
+    while process_events():
+        pygame.display.set_caption('Battleport')
+        screen.fill(black)
+        screen.blit(BackGround.image, BackGround.rect)
+
+
+        pygame.display.update()
+
+def Settings():
+    while process_events():
+        pygame.display.set_caption('Battleport')
+        screen.fill(black)
+
+
+        pygame.display.update()
+
+def Quit():
+    pygame.quit()
+
+
+
+
+#screens
+main_screen()
+Game()
+Rules()
+Settings()
+Quit()
