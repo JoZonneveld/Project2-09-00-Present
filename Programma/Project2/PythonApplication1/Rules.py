1c781f10bc720071f39b1a651da39855a3372f78
ShreyShrey96 <shreyasvp96@gmail.com>
2017-01-19 13:05:05 +0100

    .

--- Programma/Project2/PythonApplication1/Rules.py
+++ Programma/Project2/PythonApplication1/Rules.py
@@ -3,6 +3,8 @@ import pygame
 from Color import *
 def Rules(screen, BackGround, button, Proccess):
     while Proccess:
-        pygame.display.set_caption('Battleport 2')
+        pygame.display.set_caption('Battleport')
         screen.fill(black)
         screen.blit(BackGround.image, BackGround.rect)
+
+
