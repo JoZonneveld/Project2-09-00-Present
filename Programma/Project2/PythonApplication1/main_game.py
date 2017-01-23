33e953db1a3f52e285bb0a5b7524bbab2c9f7c71
Abdulvahid Kilic <a.kilic@student.tudelft.nl>
2017-01-23 12:17:12 +0100

    .

--- Programma/Project2/PythonApplication1/main_game.py
+++ Programma/Project2/PythonApplication1/main_game.py
@@ -1,6 +1,6 @@
 import pygame
 from Color import *
-
+from database import *
 def process_events():
     pygame.init()
     for event in pygame.event.get():
@@ -9,6 +9,7 @@ def process_events():
     return True
 
 def main_game(screen, button, BackGround_Game):
+    upload_score("abv", 1)
     while process_events():
         pygame.display.set_caption('Battleport')
         screen.blit(BackGround_Game.image, BackGround_Game.rect)
